39caee80a970f44d90da543243e5e7c924810b7e
Andy McKerrow <andydmckerrow@gmail.com>
2018-10-09 14:49:33 -0600

    ie test cases added

---
+++ test_plan_smallfry/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>Test Plan for Small Fry Academy Website</h1>
+
+    <h2>Contents</h2>
+    <li>Purpose</li>
+    <li>Testing Environment</li>
+    <li>Scope</li>
+    <li>Schedule</li>
+    <li>User Stories</li>
+    <li>Test Cases</li>
+    <li>Reporting</li>
+    <h2>Purpose</h2>
+    <p>The intent is to make sure the website for Small Fri Academy in Salt Lake City, UT is ready to be deployed after being developed by my collegue at Helio Training, Trea Winn</p>
+    <h2>Testing Environment</h2>
+    <p>Testing will occur while being hosted on Netlify. Site will go live to be hosted at a as-yet unknown url by mid-October 2018. </p>
+    <h2>Scope</h2>
+    <p>Client asked to have all the following tested on numerous browsers, both mobile and desktop:
+      <li>Functionality (links, navigation, etc.)</li>
+      <li>Visual display properties, including text animations</li>
+      <li>Content</li>
+     Issues outside of this scope that do not crash the page will be considered intentional.</p>
+    <h2>Schedule</h2>
+    <p><p>Testing must be completed by 5:00 pm on September 10th 2018. Please note: This means I had just 2 days to start and complete the project, from initial contact with the client to the completion of the test cycle. My intention is to deliver as much coverage as possible in the short time allowed.</p></p>
+    <h2>User Stories</h2>
+    <li>As a parent I want to find the contact information so I can arrange a visit.</li>
+    <li>As a service provider I want to find Small Fri Academy's location so I can deliver my servies.</li>
+    <li>As a parent I want to see images of students and facilities so I can decide if I want to enroll.</li>
+    <li>As the parent of a 2-year-old I want information about services available to my child so I can see if my expectations are in line with Small Fri Academy.</li>
+    <li>As a user of smartphone with no desktop or laptop, I want to navigate everywhere on the site so that I can learn about Small Fri Academy.</li>
+    <li>As a potential client I want to use the contact form so that I can start an email conversation about enrolling my child.</li>
+    <li>As someone extremely concerned about internet security issues I want to use the site on the browser of my choice so that I can maintain my privacy.</li>
+    <h2>Test Cases</h2>
+
+
+    
+    
+    
+    
+    </header>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
