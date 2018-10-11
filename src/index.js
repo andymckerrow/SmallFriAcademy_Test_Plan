128a066f71c404e4b13fa9289a4f2e354720ea15
Andy McKerrow <andydmckerrow@gmail.com>
2018-10-11 14:33:33 -0600

    commit after building for deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Test_Plan from './Test_Plan';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Test_Plan />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
