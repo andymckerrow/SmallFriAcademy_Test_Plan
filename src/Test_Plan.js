0d5b1f6f96fca0315b1eacba99451a782063fab9
Andy McKerrow <andydmckerrow@gmail.com>
2018-10-12 10:44:30 -0600

    Reports section added and sheet updated

--- src/Test_Plan.js
+++ src/Test_Plan.js
@@ -29,7 +29,7 @@ class App extends Component {
       <li>Content</li>
      Issues outside of this scope that do not crash the page will be considered intentional.</p>
     <h2>Schedule</h2>
-    <p><p>Testing must be completed by 5:00 pm on September 10th 2018. Please note: This means I had just 2 days to start and complete the project, from initial contact with the client to the completion of the test cycle. My intention is to deliver as much coverage as possible in the short time allowed.</p></p>
+    <p><p>Testing must be completed by 5:00 pm on October 10th 2018. Please note: This means I had just 2 days to start and complete the project, from initial contact with the client to the completion of the test cycle. My intention is to deliver as much coverage as possible in the short time allowed.</p></p>
     <h2>User Stories</h2>
     <li>As a parent I want to find the contact information so I can arrange a visit.</li>
     <li>As a service provider I want to find Small Fri Academy's location so I can deliver my servies.</li>
@@ -40,6 +40,8 @@ class App extends Component {
     <li>As someone extremely concerned about internet security issues I want to use the site on the browser of my choice so that I can maintain my privacy.</li>
     <h2>Test Cases</h2>
     <a href={spreadsheet} target="_blank">Download Here</a>
+    <h2>Reporting</h2>
+    <p>Share Test Cases sheet with developer by October 9th.</p>
 
 
     
